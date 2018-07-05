13aeb32017de26f1e4047d09fa99725a99ee7c96
beergeek <brett@puppet.com>
2018-07-05 20:48:19 +0800

    Modify error checking

--- tasks/el_update_pkg.rb
+++ tasks/el_update_pkg.rb
@@ -1,8 +1,11 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require 'open3'
+require 'json'
+
+params = JSON.parse(STDIN.read)
 
 begin
-  cmd_string = "yum update -y #{package_name}"
+  cmd_string = "yum update -y #{params['package_name']}"
   stdout, _stderr, _status = Open3.capture3(cmd_string)
   puts stdout unless _status != 0
 rescue => e
