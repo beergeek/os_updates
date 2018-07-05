0ac23f7eb92df4ca68961fa97c4733e21cb6a59d
beergeek <brett@puppet.com>
2018-07-05 20:44:05 +0800

    Modify error checking

--- tasks/el_checkupdate.rb
+++ tasks/el_checkupdate.rb
@@ -4,7 +4,7 @@ require 'open3'
 begin
   cmd_string = 'yum check-update'
   stdout, _stderr, _status = Open3.capture3(cmd_string)
-  puts stdout unless _status != 0
+  puts stdout
 rescue => e
   raise "Could not check for updates: #{e}"
 end
