04df4199070e6b4bbc95012107f331088c8a93d0
beergeek <brett@puppet.com>
2018-07-06 14:01:51 +0800

    New Windows

--- tasks/win_checkupdates_wsus.rb
+++ tasks/win_checkupdates_wsus.rb
@@ -1,12 +1,11 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require 'open3'
-require 'json'
 
 begin
   cmd_string = 'powershell -command "Import-Module PSWindowsUpdate"'
   stdout, stderr, status = Open3.capture3(cmd_string)
   raise 'Output not recognised', stderr if status != 0
-  cmd_string = 'powershell -command "Get-WUList -WindowsUpdate"'
+  cmd_string = 'powershell -command "Get-WUList -WindowsUpdate | Format-List -Property KB,Size,Title"'
   stdout, stderr, status = Open3.capture3(cmd_string)
   raise 'Output not recognised', stderr if status != 0
   puts stdout.strip
