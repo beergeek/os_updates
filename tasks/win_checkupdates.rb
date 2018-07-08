9d0c6e159e82e3025bc891d51ae0420a64e7c251
beergeek <brett@puppet.com>
2018-07-08 19:54:48 +0800

    Modified Windows tasks to play nicer

--- tasks/win_checkupdates.rb
+++ tasks/win_checkupdates.rb
@@ -5,6 +5,7 @@ require 'json'
 params = JSON.parse(STDIN.read)
 
 begin
+  # Determine if the PSWindowsUpdate module is installed on disk and retrieve if it is not
   if ! Dir.exists?('C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate')
     case params['module_source']
     when 'PS Gallery'
@@ -16,7 +17,7 @@ begin
       end
     when 'URL'
       if params['module_url'].nil? || params['module_url'].empty?
-        puts '`module_url` is required if URL method of retrieving the PSWindowsUpdate module is selected' 
+        puts '`module_url` is required if URL method of retrieving the PSWindowsUpdate module is selected'
         exit -1
       else
         download_cmd = "[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('#{params['module_url']}','C:\\Windows\\temp\\PSWindowsUpdate.zip')"
@@ -27,6 +28,7 @@ begin
         end
       end
     end
+    # Unzip the file
     unzip_cmd = 'powershell -c "Expand-Archive -LiteralPath C:\Windows\temp\PSWindowsUpdate.zip -DestinationPath C:\Windows\System32\WindowsPowerShell\v1.0\Modules"'
     _stdout, _stderr, _status = Opens.capture3(unzip_cmd)
     if _status != 0
@@ -34,11 +36,32 @@ begin
       exit -1
     end
   end
-  cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUList -WindowsUpdate | Format-List -Property KB,Size,Title"'
-  stdout, stderr, status = Open3.capture3(cmd_string)
-  raise "Output not recognised: #{stdout}", stderr if status != 0
-  puts stdout.strip
-  exit 0
-rescue Exception => e
-  puts "There was a problem #{e}"
+  # Find if we are using WSUS or Windows Update
+  manager_cmd = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUServiceManager | Where-Object {$_.IsManaged -eq 'true'} | foreach {$_.ServiceID}\""
+  _stdout, _stderr, _status = Open3.capture3(manager_cmd)
+  raise "Cannot get Windows Update configurations ", _stderr if _status != 0
+  # Determine which service is enable can use that to check for patches and updates
+  if _stdout
+    case _stdout.strip
+    when '3da21691-e39d-4da6-8a4b-b43877bcb1b7'
+      cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUList | Format-List -Property KB,Size,Title"'
+    when '9482f4b4-e343-43b6-b170-9a65bc822c77'
+      cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUList -WindowsUpdate | Format-List -Property KB,Size,Title"'
+    when '7971f918-a847-4430-9279-4a52d1efe18d'
+      cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUList -MicrosoftUpdate | Format-List -Property KB,Size,Title"'
+    else
+      puts 'No Update Services configured'
+      exit 0
+    end
+    # run the relevant command
+    stdout, stderr, status = Open3.capture3(cmd_string)
+    if stdout and !stdout.nil? and !stdout.empty?
+      puts stdout.strip
+    else
+      puts "No patches or updates found"
+    end
+    exit 0
+  end
+rescue
+  puts "There was a problem"
 end
