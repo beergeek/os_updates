44fc12d5d5d2833e9b07a92af98e55b1ecc5349f
beergeek <brett@puppet.com>
2018-07-08 17:38:42 +0800

    Remove the `puppet experiment`

--- tasks/win_checkupdates_wsus.rb
+++ tasks/win_checkupdates_wsus.rb
@@ -1,22 +1,12 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require 'open3'
 require 'json'
-require 'puppet'
 
 params = JSON.parse(STDIN.read)
 
 begin
   if ! Dir.exists?('C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate')
     case params['module_source']
-    when 'puppet'
-      Puppet.initialize_settings
-      @server = Puppet.settings['server']
-      x = ::Puppet::Resource.new('file', 'C:\Windows\temp\PSWindowsUpdate.zip', parameters: { ensure: 'file', source: "puppet://#{@server}/os_updates/PSWindowsUpdate.zip"})
-      _result, _report = ::Puppet::Resource.indirection.save(x)
-      if _result != 0
-        puts 'Failed to upload PSWindowsUpdate Module'
-        exit -1
-      end
     when 'PS Gallery'
       download_cmd = '[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile("https://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/47/PSWindowsUpdate.zip","C:\Windows\temp\PSWindowsUpdate.zip")'
       _stdout, _stderr, _status = Opens.capture3(download_cmd)
