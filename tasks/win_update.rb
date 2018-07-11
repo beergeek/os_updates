cdedb00583ecbf29ea926087365eb109b88ddea0
beergeek <brett@puppet.com>
2018-07-11 08:35:42 +0800

    Add more Windows tasks

--- tasks/win_update.rb
+++ tasks/win_update.rb
@@ -54,14 +54,17 @@ begin
   _stdout, _stderr, _status = Open3.capture3(manager_cmd)
   raise "Cannot get Windows Update configurations ", _stderr if _status != 0
   # Determine which service is enable can use that to apply patches and updates
+  if params['allow_reboot'] == false {
+    _allow_reboot = '-IgnoreReboot'
+  }
   if _stdout
     case _stdout.strip
     when '3da21691-e39d-4da6-8a4b-b43877bcb1b7'
-      cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUInstall -AcceptAll | Format-List -Property KB,Size,Title"'
+      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -AcceptAll #{_allow_reboot}\""
     when '9482f4b4-e343-43b6-b170-9a65bc822c77'
-      cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUInstall -WindowsUpdate -AcceptAll | Format-List -Property KB,Size,Title"'
+      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -WindowsUpdate -AcceptAll #{_allow_reboot}\""
     when '7971f918-a847-4430-9279-4a52d1efe18d'
-      cmd_string = 'powershell -command "Import-Module PSWindowsUpdate; Get-WUInstall -MicrosoftUpdate -AcceptAll | Format-List -Property KB,Size,Title"'
+      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -MicrosoftUpdate -AcceptAll #{_allow_reboot}\""
     else
       puts 'No Update Services configured'
       exit 0
