af8c3cddb952eb9756d4f646497d1bdf6dd42f61
beergeek <brett@puppet.com>
2018-10-30 15:15:29 +0800

    New tasks

--- tasks/el_pin_package.rb
+++ tasks/el_pin_package.rb
@@ -7,14 +7,14 @@ params = JSON.parse(STDIN.read)
 def ensure_yumversion
   cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
   stdout, _stderr, status = Open3.capture3(cmd_string)
-  puts stdout unless status != 0
+  raise "Could not install `yum-plugin-versionlock`: #{e.message}" unless status != 0
 rescue => e
   raise "Could not install `yum-plugin-versionlock`: #{e.message}"
 end
 
 begin
-  ensure_yumversion
-  cmd_string = "yum update -y #{params['package_name']}"
+  ensure_yumversion()
+  cmd_string = "yum versionlock #{params['package_name']}"
   stdout, _stderr, status = Open3.capture3(cmd_string)
   puts stdout unless status != 0
 rescue => e
--- tasks/el_show_pinned.rb
+++ tasks/el_show_pinned.rb
@@ -1,28 +1,20 @@
-#!/bin/sh
+#!/opt/puppetlabs/puppet/bin/ruby
+require 'open3'
+require 'json'
 
-# Puppet Task Name: 
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
+def ensure_yumversion
+  cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
+  stdout, _stderr, status = Open3.capture3(cmd_string)
+  raise "Could not install `yum-plugin-versionlock`: #{e.message}" unless status != 0
+rescue => e
+  raise "Could not install `yum-plugin-versionlock`: #{e.message}"
+end
+
+begin
+  ensure_yumversion()
+  cmd_string = "yum versionlock list"
+  stdout, _stderr, status = Open3.capture3(cmd_string)
+  puts stdout unless status != 0
+rescue => e
+  raise "Could not list pinned: #{e.message}"
+end
