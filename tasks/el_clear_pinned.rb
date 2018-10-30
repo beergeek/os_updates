ec65bb1ee7a0da6915df950a0e41159c87870bfc
beergeek <brett@puppet.com>
2018-10-30 15:08:00 +0800

    New tasks

---
+++ tasks/el_clear_pinned.rb
@@ -0,0 +1,20 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+require 'open3'
+require 'json'
+
+def ensure_yumversion
+  cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
+  stdout, _stderr, status = Open3.capture3(cmd_string)
+  puts stdout unless status != 0
+rescue => e
+  raise "Could not install `yum-plugin-versionlock`: #{e.message}"
+end
+
+begin
+  ensure_yumversion()
+  cmd_string = "yum versionlock clear"
+  stdout, _stderr, status = Open3.capture3(cmd_string)
+  puts stdout unless status != 0
+rescue => e
+  raise "Could not clear pinned: #{e.message}"
+end
---
+++ tasks/el_pin_package.rb
@@ -0,0 +1,22 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+require 'open3'
+require 'json'
+
+params = JSON.parse(STDIN.read)
+
+def ensure_yumversion
+  cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
+  stdout, _stderr, status = Open3.capture3(cmd_string)
+  puts stdout unless status != 0
+rescue => e
+  raise "Could not install `yum-plugin-versionlock`: #{e.message}"
+end
+
+begin
+  ensure_yumversion
+  cmd_string = "yum update -y #{params['package_name']}"
+  stdout, _stderr, status = Open3.capture3(cmd_string)
+  puts stdout unless status != 0
+rescue => e
+  raise "Could not update: #{e.message}"
+end
