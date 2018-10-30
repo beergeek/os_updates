02da4e395b1b7100c37922455bc65e4d7849e253
beergeek <brett@puppet.com>
2018-10-30 15:20:44 +0800

    New tasks

--- tasks/el_pin_package.rb
+++ tasks/el_pin_package.rb
@@ -7,7 +7,7 @@ params = JSON.parse(STDIN.read)
 def ensure_yumversion
   cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
   stdout, _stderr, status = Open3.capture3(cmd_string)
-  raise "Could not install `yum-plugin-versionlock`: #{e.message}" unless status != 0
+  raise "Could not install `yum-plugin-versionlock`: #{e.message}" unless status == 0
 rescue => e
   raise "Could not install `yum-plugin-versionlock`: #{e.message}"
 end
