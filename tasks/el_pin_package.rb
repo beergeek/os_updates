7f44761155cab93ec31ddb485bb6e98fa4d26acf
beergeek <brett@puppet.com>
2018-10-31 16:47:06 +0800

    New version of PDK

--- tasks/el_pin_package.rb
+++ tasks/el_pin_package.rb
@@ -7,16 +7,16 @@ params = JSON.parse(STDIN.read)
 def ensure_yumversion
   cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
   _stdout, _stderr, status = Open3.capture3(cmd_string)
-  raise "Could not install `yum-plugin-versionlock`: #{e.message}" unless status == 0
+  raise 'Could not install `yum-plugin-versionlock`' unless status.zero?
 rescue => e
   raise "Could not install `yum-plugin-versionlock`: #{e.message}"
 end
 
 begin
-  ensure_yumversion()
+  ensure_yumversion
   cmd_string = "yum versionlock #{params['package_name']}"
   _stdout, _stderr, status = Open3.capture3(cmd_string)
-  puts "Could pin package(s)" unless status == 0
+  puts 'Could pin package(s)' unless status == 0
   list_string = 'yum versionlock list'
   stdout, _stderr, status = Open3.capture3(list_string)
   puts stdout unless status != 0
--- tasks/el_show_pinned.rb
+++ tasks/el_show_pinned.rb
@@ -5,13 +5,13 @@ require 'json'
 def ensure_yumversion
   cmd_string = 'puppet resource package yum-plugin-versionlock ensure=present'
   _stdout, _stderr, status = Open3.capture3(cmd_string)
-  raise "Could not install `yum-plugin-versionlock`: #{e.message}" unless status == 0
+  raise 'Could not install `yum-plugin-versionlock`' unless status.zero?
 rescue => e
   raise "Could not install `yum-plugin-versionlock`: #{e.message}"
 end
 
 begin
-  ensure_yumversion()
+  ensure_yumversion
   cmd_string = "yum versionlock list"
   stdout, _stderr, status = Open3.capture3(cmd_string)
   puts stdout unless status != 0
